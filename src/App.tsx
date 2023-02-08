a0e77498a1b3d5b36360a039004f82f269058901
eorev <ethanorev@gmail.com>
2023-02-08 18:52:38 -0500

    Added Header, Button, Columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,12 +8,32 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <header className="headerTwo">
+                <h1>This is my second header.</h1>
+                <img src="https://picsum.photos/20/30" alt="lorem picsum" />
+                <ul className="headerTwo__list">
+                    <li>First item</li>
+                    <li>Second item</li>
+                    <li>Third item</li>
+                </ul>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Click Me!
+                </Button>
+            </header>
             <h1>Ethan Orevillo</h1>
             <h2>Hello World</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>First Column</h1>
+                        <div className="rectangle"></div>
+                    </Col>
+                    <Col>
+                        <h1>Second Column</h1>
+                        <div className="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
