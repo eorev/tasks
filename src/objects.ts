5563663759edf5b27806d0e6d905e1115001dc45
eorev <eorev@udel.edu>
2023-03-03 19:54:21 -0800

    Solved isValid and returning a valid question

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,7 +30,11 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return question.expected.toLowerCase().trim() === answer.toLowerCase().trim();
+    } else if (question.type === "multiple_choice_question") {
+        return question.expected === answer;
+    }
 }
 
 /**
