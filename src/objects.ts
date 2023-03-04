742ef4308cad67a427cad5003f5051b55676f32e
eorev <eorev@udel.edu>
2023-03-03 20:30:40 -0800

    Solved duplicateQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -106,7 +106,14 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    let newQuestion: Question = question;
+    if (question.published) {
+        newQuestion.published = false;
+    }
+    else {
+        newQuestion.published = true;
+    }
+    return newQuestion;
 }
 
 /**
@@ -116,7 +123,11 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    let newQuestion: Question = oldQuestion;
+    newQuestion.id = id;
+    newQuestion.name = "Copy of " + oldQuestion.name;
+    newQuestion.published = false;
+    return newQuestion;
 }
 
 /**
