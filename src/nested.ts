f8f854143ee51d1b4577c985f7a6ce9171ecd480
eorev <eorev@udel.edu>
2023-03-12 17:20:26 -0700

    sumPublishedPoints passed

--- src/nested.ts
+++ src/nested.ts
@@ -58,7 +58,12 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions.map((question) => {
+        if (question.published) {
+            return question.points
+        }
+        return 0
+    }).reduce((sum, cur) => sum + cur, 0)
 }
 
 /***
